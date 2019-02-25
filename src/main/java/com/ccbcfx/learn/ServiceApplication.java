e1a0d94dfdc6478e453d2109e93f2fb8a562021a
luzhiqing <luzhiqing@ccbtrust.com.cn>
2019-02-25 20:32:50 +0800

    修改下划线

--- src/main/java/com/ccbcfx/learn/ServiceApplication.java
+++ src/main/java/com/ccbcfx/learn/ServiceApplication.java
@@ -2,11 +2,12 @@ package com.ccbcfx.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class Application {
+@EnableEurekaClient
+public class ServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
-
+        SpringApplication.run(ServiceApplication.class,args);
     }
 }
